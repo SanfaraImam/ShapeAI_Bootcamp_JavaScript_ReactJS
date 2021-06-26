1dfbec38e554c1e8fe87d6acac3d5a04aa2fb3e5
SanfaraImam <86536275+SanfaraImam@users.noreply.github.com>
2021-06-27 02:45:03 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>7 Days Free Bootcamp on Basic Web Dev React JS</p>
+    </div>
+  );
+}
+
+export default Info;
